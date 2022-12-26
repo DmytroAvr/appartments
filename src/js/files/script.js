f0c542c350ab90ce3b8c77aff578184ec3d4a605
Dmytro <dmytro9avr@gmail.com>
2022-12-26 16:52:51 +0200

    refactor: rebase icons and iconFonts

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,22 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+
+/*
+<form name="myForm" action="data_handler.php">
+<input type="tel" name="telephone">
+<input type="tel" name="mobile">
+<input type="button" value="Submit" onclick="validateAndSend()">
+</form>
+	*/
+//const contactsForm = document.querySelector(".contact-form");
+function validateAndSend() {
+	 if (contactsForm.formtel.value == '' && contactsForm.formemail.value == '') {
+		  alert('You have to enter at least one phone number.');
+		  return false;
+	 }
+	 else {
+		  myForm.submit();
+	 }
+}
